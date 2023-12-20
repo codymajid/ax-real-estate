9fc723c5ffbcecdd534b915293233d64f73b2fa4
Majid Aziz <azizmajid34@gmail.com>
2023-12-20 22:51:56 +0500

    define colors

--- Components/navbar/Navbar.tsx
+++ Components/navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './Navbar.module.scss';
 import menu_items from './navbar.json';
-import { KeyboardArrowDownOutlined, KeyboardArrowUp } from '@mui/icons-material';
+import { Bookmark, KeyboardArrowDownOutlined, KeyboardArrowUp, Settings, WhatsApp } from '@mui/icons-material';
 import Image from 'next/image';
 
 
@@ -15,16 +15,23 @@ const Navbar = () => {
     return (
         <div className={styles.wrapper}>
             <div className={styles.headerLogo}>
-                <Image src="https://images.pexels.com/photos/1162361/pexels-photo-1162361.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt='header-logo' className={styles.headerLogoImage} />
+                <img src="https://images.pexels.com/photos/1162361/pexels-photo-1162361.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt='header-logo' loading='lazy' className={styles.headerLogoImage} width={50} height={50} />
             </div>
-            <ul>
+            <ul className={styles.list_items}>
                 {menu_items.map((item : any) => {
                     return (<>
-                        <li> {item.title} </li>
-                        <span> {icons[item.icon]} </span>
+                        <li className={styles.single_item}> {item.title} <span className={styles.list_icon}> {icons[item.icon]} </span> </li>
                     </>)
                 })}
             </ul>
+            <div className={styles.iconWrapper}>
+                <span className={styles.lang}> En </span>
+                <span> <Settings /> </span>
+                <span> <Bookmark /> </span>
+                <span> <WhatsApp /> </span>
+                <span> FOLLOW US </span>
+                <span> CALL US </span>
+            </div>
         </div>
     )
 }
